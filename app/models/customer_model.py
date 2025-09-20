5d0b2056b3f1cbcfac21889bce1700e8f3899b51
JoaoSantos6 <78502928+JoaoSantos6@users.noreply.github.com>
2025-09-20 15:57:36 -0300

    mapeamento da tabela costumers

---
+++ app/models/customer_model.py
@@ -0,0 +1,21 @@
+import sqlalchemy as sa
+import sqlalchemy.orm
+
+Base = sqlalchemy.orm.declarative_base()
+
+class CustomerModel(Base):
+    __tablename__ = 'costumers'
+    
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    cpf = sa.Column(sa.String(14), nullable=False)
+    email = sa.Column(sa.String(255), nullable=False)
+    full_name = sa.Column(sa.String(255), nullable=False)
+    birthday = sa.Column(sa.Date, nullable=False)
+    phone_number = sa.Column(sa.String(20), nullable=False)
+    balance = sa.Column(sa.Numeric(precision=12, scale=2), nullable=False)
+    currency = sa.Column(sa.String(3), nullable=False) 
+    account_number = sa.Column(sa.String(20), nullable=False)
+    is_active = sa.Column(sa.Boolean, nullable=False, default=True)
+    is_verified = sa.Column(sa.Boolean, nullable=False, default=False)
+    created_at = sa.Column(sa.DateTime, nullable=False, server_default=sa.func.now())
+    updated_at = sa.Column(sa.DateTime, nullable=False, server_default=sa.func.now(), onupdate=sa.func.now())
